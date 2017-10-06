b073112a781a8eb02cceaac3cc4da551d7dd6377
Ryan Johnston <ryanjohnston@ambush.ryanj.org>
2017-10-06 10:59:29 -0600

    Added clean.sh

---
+++ scripts/clean.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+source 'scripts/common.sh'
+
+OLD_GITCONFIG="git/gitconfig.local.symlink"
+
+info "clean.sh - Start"
+
+if [ -f $OLD_GITCONFIG ]
+then
+  info "Removing $OLD_GITCONFIG"
+  rm $OLD_GITCONFIG
+else
+  info "$OLD_GITCONFIG not found."
+fi
+
+info "clean.sh - End"
