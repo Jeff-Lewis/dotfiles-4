541298d2b9de2b4fd22fb478a712018fd3439fb0
Ryan Johnston <ryanj@ryanj.org>
2017-10-06 09:10:49 -0600

    Added homebrew install script

---
+++ homebrew/install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# Homebrew
+#
+# This installs some of the common dependencies needed (or at least desired)
+# using Homebrew.
+
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+
+  # Install the correct homebrew for each OS type
+  if test "$(uname)" = "Darwin"
+  then
+    ruby -e "$(curl -fsSL Homebrew/install:install@master (raw))"
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+  then
+    ruby -e "$(curl -fsSL Linuxbrew/install:install@master (raw))"
+  fi
+
+fi
+
+exit 0
