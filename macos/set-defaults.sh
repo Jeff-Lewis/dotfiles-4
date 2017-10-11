adeef08df53b840c00913b96d4d4e1138f837798
Ryan Johnston <ryanj@ryanj.org>
2017-10-11 08:28:30 -0600

    Add more tweaks to macos.

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -7,35 +7,91 @@
 
 # Run ./set-defaults.sh and you'll be good to go.
 
-# Disable press-and-hold for keys in favor of key repeat.
-defaults write -g ApplePressAndHoldEnabled -bool false
+set -e
 
-# Use AirDrop over every interface. srsly this should be a default.
-defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+source ./script/common.sh
 
-# Always open everything in Finder's list view. This is important.
+# ------------ DISPLAY -------------------------
+info 'DISPLAY SETTINGS'
+
+info 'Enable sub-pixel rendering on non-Apple displays.'
+defaults write NSGlobalDomain AppleFontSmoothing -int 2
+
+# ------------ SYSTEM --------------------------
+info 'SYSTEM SETTINGS'
+
+info 'Disable auto-correct.'
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+info 'Disable animations for windows.'
+defaults write com.apple.finder DisableAllAnimations -bool true
+
+info 'Screensaver always asks for password.'
+defaults write com.apple.screensaver askForPassword -int 1
+
+info 'Reset launchpad.'
+[ -e ~/Library/Application\ Support/Dock/*.db ] && rm ~/Library/Application\ Support/Dock/*.db
+
+# ------------ FINDER --------------------------
+info 'FINDER SETTINGS'
+
+info 'Always open finder in list view.'
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
-# Show the ~/Library folder.
+info 'Do not hide the ~/Library folder.'
 chflags nohidden ~/Library
 
-# Set a really fast key repeat.
-# defaults write NSGlobalDomain KeyRepeat -int 1
+info 'Always show file extensions.'
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-# Set the Finder prefs for showing a few different volumes on the Desktop.
+info 'Show the status bar at the bottom of finder windows.'
+defaults write com.apple.finder ShowStatusBar -bool true
+
+info 'Always securely delete the trash'
+defaults write com.apple.finder EmptyTrashSecurely -bool true
+
+info 'Use full path as finder window title.'
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+info 'Disable disk image validation.'
+defaults write com.apple.frameworks.diskimages skip-verify -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
+
+info 'Show external drives and volumes on the desktop.'
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
-# Run the screensaver if we're in the bottom-left hot corner.
+info 'Enable Airdrop on all interfaces.'
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+
+info 'Disable resume system-wide.'
+defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
+
+# ------------- HOT CORNERS --------------------
+info 'DESKTOP SETTINGS'
+
+info 'Turn on the screen saver when mouse is at bottom left of display.'
 defaults write com.apple.dock wvous-bl-corner -int 5
 defaults write com.apple.dock wvous-bl-modifier -int 0
 
-# Hide Safari's bookmark bar.
+# ------------- SAFARI -------------------------
+info 'SAFARI SETTINGS'
+
+info 'Hide Safari bookmark bar by default.'
 defaults write com.apple.Safari ShowFavoritesBar -bool false
 
-# Set up Safari for development.
+info 'Enable development features in Safari.'
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+info 'Disable thumbnails for last and most visited sites.'
+defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
+
+info 'Enable debug window.'
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+
+
